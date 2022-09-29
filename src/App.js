f82d08386336e240f03d6a56053ed984adcd17b6
beomJoon <104752645+joon-github@users.noreply.github.com>
2022-09-29 16:20:32 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import styled from "styled-components";
+import { useEffect } from "react";
 import Main from "./pages/Main";
 import Nav from "./components/Nav";
+import Header from "./components/Header";
 function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+  const App = styled.div`
+    display: flex;
+    height: calc(var(--vh, 1vh) * 100);
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  `;
   return (
-    <div className="App">
+    <App>
+      <Header></Header>
       <Main></Main>
       <Nav></Nav>
-    </div>
+    </App>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,10 @@
 import React from "react";
+// import styled from "styled-components";
 
 const Nav = () => {
+  // const NavTag = styled.nav`
+  //   background-color: red;
+  // `;
   return <div>nav</div>;
 };
 
